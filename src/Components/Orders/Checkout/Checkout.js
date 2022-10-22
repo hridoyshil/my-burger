aad1c3092752170b5bd316c0d404a986ddd040d3
Hridoy <hriodykumarshil12@gmail.com>
2022-10-22 23:05:42 +0600

    Saving Orders to Database

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
+import axios from 'axios';
+
+import { connect } from 'react-redux';
+
+const mapStateTopProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
+
 class Checkout extends Component {
     state = {
         values: {
@@ -24,19 +36,33 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.props.totalPrice,
+            orderTime: new Date(),
+        }
+        axios.post("https://my-burger-8e7de-default-rtdb.firebaseio.com//orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <div>
+                <h4 style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}>Payment: {this.props.totalPrice} BDT</h4>
                 <form style={{
                     border: "1px solid grey",
                     boxShadow: "1px 1px #888888",
                     borderRadius: "5px",
-                    padding: "30px",
+                    padding: "20px",
                     margin: "30px",
-                }} >
+                }}>
                     <textarea name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Your Address" onChange={(e) => this.inputChangerHandler(e)}></textarea>
                     <br />
                     <input name="phone" className="form-control" value={this.state.values.phone} placeholder="Your Phone Number" onChange={(e) => this.inputChangerHandler(e)} />
@@ -49,9 +75,11 @@ class Checkout extends Component {
                     <Button style={{ backgroundColor: "#D70F64" }} className="mr-auto" onClick={this.submitHandler}>Place Order</Button>
                     <Button color="secondary" className="ml-1" onClick={this.goBack}>Cancel</Button>
                 </form>
-            </div >
+            </div>
         )
     }
 }
 
 export default Checkout;
+
+// export default connect(mapStateTopProps)(Checkout);
