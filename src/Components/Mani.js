8341bdd810155314b74d66fcbab2d13ab24f4790
Hridoy <hriodykumarshil12@gmail.com>
2022-10-22 07:42:30 +0600

    Burger Builder Project --React-- Project Setup

---
+++ src/Components/Mani.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/Header";
+import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
+
+const Mani = props => {
+    return (
+        <div>
+            < Header />
+            <BurgerBuilder />
+        </div>
+    )
+}
+
+export default Mani;
